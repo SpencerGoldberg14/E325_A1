4bf28d52bed5bf8f6952c06d91391f75df8a722f
SpencerGoldberg14 <smgoldbe@hamilton.edu>
2024-09-05 15:47:53 +0000

    started code

--- Activity 1.R
+++ Activity 1.R
@@ -1,1 +1,34 @@
 # start of activity and homework script
+
+6^6
+
+5+10
+7*1
+
+aNumber <- 49280
+
+aNumber/2
+# elevation in ft
+peaks <- c(5344,5114,4960)
+
+#convert to meters
+peaks.m <- peaks/3.281
+1:4
+# prominence in ft
+prom <- c(4914,2100,840)
+
+# difference between peak height and prominence
+peaks - prom
+
+# create a character vector peak name
+peakNames <- c("Mount Marcy", "Algonquin Peak", "Mount Haystack")
+
+peaks[1:2]
+
+highPeaks <- data.frame(elev = peaks,
+                        prom = prom,
+                        name = peakNames)
+
+highPeaks[1,1]
+highPeaks[ ,1]
+highPeaks[1:2]
