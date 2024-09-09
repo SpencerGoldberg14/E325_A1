dbc84c6350f9cc0d508ac8f2bb1c2da72e292a2e
SpencerGoldberg14 <smgoldbe@hamilton.edu>
2024-09-09 17:40:21 +0000

    Classwork and HW 9/9

--- Activity 1.R
+++ Activity 1.R
@@ -1,33 +1,33 @@
 # start of activity and homework script
 
-# create a vector snow depth
+# prompt 1: create a vector snow depth
 snowDepth <- c(2.5, 3.5, 4.5)
 
-# convert snow depth from in to cm
+# prompt 1: convert snow depth from in to cm
 snowDepth.cm <- snowDepth*2.54
 
-# elevation in ft
+# prompt 2: elevation in ft
 peaks <- c(4960, 4926, 4857, 4840)
 
-# climb ascent in ft
+# prompt 2: climb ascent in ft
 climbAscent <- c(3570, 4265, 2800, 4178)
 
-# round trip length in mi
+# prompt 2: round trip length in mi
 roundTrip <- c(17.8, 17.9, 13.2, 16)
 
-# create a character vector peak name
+# prompt 2: create a character vector peak name
 peakNames <- c("Mount Haystack", "Mount Skylight", "Dix Mountain", "Gray Peak")
 
-# create a data frame 
+# prompt 2: create a data frame 
 highPeaks <- data.frame(elev = peaks,
                         climbAscent = climbAscent,
                         roundTrip= roundTrip,
                         name = peakNames)
 
-# create vector degrees Celsius
+# prompt 3: create vector degrees Celsius
 degreesC <- c(-44, 0, 20, 35)
 
-# convert degrees Celsius to degrees Farenheit
+# prompt 3: convert degrees Celsius to degrees Farenheit
 degreesF <- degreesC*(9/5)+32
 
 # question 1: convert elevation from feet to meters
@@ -38,4 +38,6 @@ peaks.m <- peaks*0.3048
 
 # question 3: isolate the row with the longest round trip length
 max.roundTrip <- max(roundTrip)
+
+highPeaks[2,]
 
